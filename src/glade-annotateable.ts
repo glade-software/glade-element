597ec030e124be4dd901b0b188f63013e95f397b
Matt Johnston <m@ttjohnston.com>
2020-05-22 04:17:06 -0300

    annotations are fetched from db

--- src/glade-annotateable.ts
+++ src/glade-annotateable.ts
@@ -13,7 +13,7 @@
  */
 
 import {LitElement, html, customElement, property, css} from 'lit-element';
-
+import firebase from 'firebase';
 /**
  * An example element.
  *
@@ -31,15 +31,22 @@ export class GladeAnnotateable extends LitElement {
    * The slug used to fetch the Glade document
    */
   @property({type: String})
-  slug = null;
+  slug = '';
 
-  annotations = [
-    {
-      gladeIndex: 3,
-      body: 'this is some body text',
-      postedBy: 'matt',
-    },
-  ];
+  // traditionally should not be published, but the embeddable nature of <glade-component> seems to be an exception
+  firebaseConfig = {
+    apiKey: 'AIzaSyAtc2ed5rsHT7IOF9E1psFhkqtCqKib25U',
+    authDomain: 'glade-software-firebase.firebaseapp.com',
+    databaseURL: 'https://glade-software-firebase.firebaseio.com',
+    projectId: 'glade-software-firebase',
+    storageBucket: 'glade-software-firebase.appspot.com',
+    messagingSenderId: '527964919900',
+    appId: '1:527964919900:web:dc1ffc9e14a70b08b3ae99',
+  };
+
+  db!: firebase.firestore.Firestore;
+
+  annotations = [];
 
   constructor() {
     super();
@@ -55,18 +62,45 @@ export class GladeAnnotateable extends LitElement {
     }
   `;
 
-  initializeFirebase() {}
+  initializeFirebase() {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(this.firebaseConfig);
+    }
+    this.db = firebase.firestore();
+  }
 
-  showAnnotations(gladeIndex: number) {
+  async getAnnotationsFromDB() {
+    if (this.slug.length) {
+      const annotationsSnapshots = await this.db
+        .collection('glade-trees')
+        .doc(this.slug)
+        .collection('annotations')
+        .get();
+
+      annotationsSnapshots.forEach((document) => {
+        const {body, postedBy, domNodeIndex} = document.data();
+        console.log('document.data()', document.data());
+        this.annotations.push({
+          body,
+          postedBy,
+          gladeDomNodeIndex: domNodeIndex,
+        });
+      });
+    }
+  }
+
+  showAnnotations(gladeDomNodeIndex: number) {
     console.log(
       this.annotations.filter(
-        (annotation) => annotation.gladeIndex === gladeIndex
+        (annotation) => annotation.gladeDomNodeIndex === gladeDomNodeIndex
       )
     );
   }
 
-  connectedCallback() {
+  async connectedCallback() {
     super.connectedCallback();
+    this.initializeFirebase();
+    await this.getAnnotationsFromDB();
 
     console.log(
       `there are ${this.gladeContentNodes.length} DOM nodes descending from this component`
@@ -75,9 +109,11 @@ export class GladeAnnotateable extends LitElement {
     console.log(this.slug);
 
     this.gladeContentNodes.forEach((node, idx) => {
-      const annotationsForIndex = this.annotations.filter(({gladeIndex}) => {
-        return gladeIndex === idx;
-      });
+      const annotationsForIndex = this.annotations.filter(
+        ({gladeDomNodeIndex}) => {
+          return gladeDomNodeIndex === idx;
+        }
+      );
 
       if (annotationsForIndex.length) {
         node.classList.add('glade-has-annotations');
@@ -88,20 +124,17 @@ export class GladeAnnotateable extends LitElement {
 
       node.setAttribute('data-glade-index', `${idx}`);
     });
-
-    this.initializeFirebase();
   }
 
   render() {
     return html`<slot
-        @mouseup=${(ev) => {
-          const targetNode = ev.path[0];
-          const gladeIndex = targetNode.getAttribute('data-glade-index');
+      @mouseup=${(ev) => {
+        const targetNode = ev.path[0];
+        const gladeDomNodeIndex = targetNode.getAttribute('data-glade-index');
 
-          console.log('mouseup @ gladeIndex', gladeIndex);
-        }}
-      ></slot>
-      <div></div> `;
+        console.log('mouseup @ gladeIndex', gladeDomNodeIndex);
+      }}
+    ></slot>`;
   }
 }
 
