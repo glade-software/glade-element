2bf60818eddfba6f2eb8c7c0f2c522f5aecab03c
Matt <m@ttjohnston.com>
2020-11-06 08:48:29 -0400

    made the homepage content annotateable

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -5,89 +5,71 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
+import Head from '@docusaurus/Head';
 
 const features = [
   {
     title: <>Step 1</>,
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
-    description: (
-      <>
-        Include the JS Library
-      </>
-    ),
+    description: <>Include the JS Library</>,
   },
   {
     title: <>Step 2</>,
     imageUrl: 'img/undraw_docusaurus_tree.svg',
-    description: (
-      <>
-        Wrap your content
-      </>
-    ),
+    description: <>Wrap your content</>,
   },
   {
     title: <>Step 3</>,
     imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Annotate!
-      </>
-    ),
+    description: <>Annotate!</>,
   },
 ];
-
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={classnames('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-}
 
 function Home() {
   const context = useDocusaurusContext();
   const {siteConfig = {}} = context;
   return (
-    <Layout
-      title={`Glade`}
-      description="Docs  <glade-annotateable>">
-      <header className={classnames('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={classnames(
-                'button button--outline button--tertiary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/getting-started')}>
-              Get Started
-            </Link>
+    <>
+      <Layout title={`Glade`} description="Docs  <glade-annotateable>">
+        <Head>
+          <script
+            type="module"
+            src="https://unpkg.com/@glade-software/glade-annotateable"
+          ></script>
+        </Head>
+        <header className={classnames('hero hero--primary', styles.heroBanner)}>
+          <div className="container">
+            <h1 className="hero__title">{siteConfig.title}</h1>
+            <p className="hero__subtitle">{siteConfig.tagline}</p>
+            <div className={styles.buttons}>
+              <Link
+                className={classnames(
+                  'button button--outline button--tertiary button--lg',
+                  styles.getStarted
+                )}
+                to={useBaseUrl('docs/getting-started')}
+              >
+                Get Started
+              </Link>
+            </div>
           </div>
-        </div>
-      </header>
-      <main>
-        {features && features.length && (
+        </header>
+        <main>
           <section className={styles.features}>
             <div className="container">
               <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
+                <glade-annotateable>
+                  <p>
+                    This homepage content is annotateable using Glade's open annotation
+                    platform!
+                  </p>
+                </glade-annotateable>
               </div>
             </div>
           </section>
-        )}
-      </main>
-    </Layout>
+        </main>
+      </Layout>
+    </>
   );
 }
 
