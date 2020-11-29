0100a61e349f0fb006c9838014d5d9beb2911cf2
Matt <m@ttjohnston.com>
2020-11-29 06:36:05 -0400

    first tests

--- src/test/glade-annotatable_test.ts
+++ src/test/glade-annotatable_test.ts
@@ -1,10 +1,67 @@
 import {GladeAnnotatable} from '../glade-annotatable.js';
+import {fixture, expect, html, aTimeout} from '@open-wc/testing';
 
 const assert = chai.assert;
 
 suite('glade-annotatable', () => {
   test('is defined', () => {
-    const el = document.createElement('glade-annotatable');
+    const el: GladeAnnotatable = document.createElement('glade-annotatable');
     assert.instanceOf(el, GladeAnnotatable);
+  });
+
+  test('gladedocumenthash attribute is "0" if there is no content', async () => {
+    const el: GladeAnnotatable = await fixture(
+      html` <glade-annotatable></glade-annotatable> `
+    );
+    expect(el).dom.to.equal(
+      `<glade-annotatable gladedocumenthash="0"></glade-annotatable>`
+    );
+  });
+
+  test('constant hashes are generated as expected', async () => {
+    const knownGladeDomNodeHash = 138399344; // hashString('<p>some known content</p>')
+    const knownGladeDocumentHash = -6866858; // hashString('<p data-glade-node-hash="138399344">some known content</p>')
+
+    const el: GladeAnnotatable = await fixture(
+      html`<glade-annotatable><p>some known content</p></glade-annotatable>`
+    );
+
+    expect(el).dom.to.equal(
+      `
+      <glade-annotatable gladedocumenthash="${knownGladeDocumentHash}">
+        <p data-glade-node-hash="${knownGladeDomNodeHash}">some known content</p>
+      </glade-annotatable>
+      `
+    );
+  });
+
+  test('element is not verbose by default', async () => {
+    const el: GladeAnnotatable = await fixture(
+      html`<glade-annotatable><p>some known content</p></glade-annotatable>`
+    );
+    expect(el.verbose).to.be.false;
+  });
+
+  test('element is verbose when "verbose" attribute is present', async () => {
+    const el: GladeAnnotatable = await fixture(
+      html`<glade-annotatable verbose
+        ><p>some known content</p></glade-annotatable
+      >`
+    );
+    expect(el.verbose).to.be.true;
+  });
+
+  test('annotations are present for the homepage content', async () => {
+    const el: GladeAnnotatable = await fixture(html`
+      <glade-annotatable verbose>
+        <p>
+          This homepage content is annotatable using Glade's open annotation
+          platform!
+        </p>
+      </glade-annotatable>
+    `);
+    await aTimeout(1000);
+    console.log(el.annotations);
+    expect(el.annotations.length).to.be.greaterThan(0);
   });
 });
