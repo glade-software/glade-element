d77c263d71a2c67820ecee822021618c7f239658
Matt <m@ttjohnston.com>
2020-12-28 15:16:34 -0400

    redirect on signup is working - does not maintain session

--- app/src/pages/signup.js
+++ app/src/pages/signup.js
@@ -32,7 +32,8 @@ const validationSchema = yup.object({
 const Signup = () => {
   const router = useRouter();
   const { currentUser } = useContext(AuthContext);
-
+  const qs = router.query?.from ? `?from=${encodeURIComponent(router.query?.from)}` : '';
+  console.log(qs)
   return (
     <Grommet theme={grommet}>
       <Box align="center">
@@ -54,7 +55,7 @@ const Signup = () => {
                   .createUserWithEmailAndPassword(email, password)
                   .then(() => {
                     console.log("user created");
-                    router.replace('/profile');
+                    router.replace(`/profile${qs}`);
                   })
                   .catch(function (error) {
                     // Handle Errors here.
