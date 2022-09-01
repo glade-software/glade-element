297ed9d8a8cab57400192acd5e231a2761b79214
Matt Johnston <m@ttjohnston.com>
2022-09-01 20:43:51 -0300

    add algolia

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,95 +1,100 @@
 module.exports = {
-  title: '<glade-annotatable>',
-  tagline: 'make any webpage annotatable',
-  url: 'https://docs.glade.app',
-  baseUrl: '/',
-  favicon: 'img/favicon.ico',
-  organizationName: 'glade-software', // Usually your GitHub org/user name.
-  projectName: 'glade-annotatable', // Usually your repo name.
+  title: "<glade-annotatable>",
+  tagline: "make any webpage annotatable",
+  url: "https://docs.glade.app",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: "glade-software", // Usually your GitHub org/user name.
+  projectName: "glade-annotatable", // Usually your repo name.
   themeConfig: {
+    algolia: {
+      appId: 'DM7GDZHQA2',
+      apiKey: '679fd8035519b32d53f2e3035fc1477c',
+      indexName: 'glade',
+    },
     navbar: {
-      title: 'Glade',
+      title: "Glade",
       logo: {
-        alt: 'Glade Software logo',
-        src: 'img/glade-rounded.png',
+        alt: "Glade Software logo",
+        src: "img/glade-rounded.png",
       },
       items: [
         {
-          to: 'docs/getting-started',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
+          to: "docs/getting-started",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left",
         },
-        {href: 'https://demos.glade.app', label: 'Demos', position: 'left'},
+        { href: "https://demos.glade.app", label: "Demos", position: "left" },
         {
-          href: 'https://github.com/glade-software/glade-element',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/glade-software/glade-element",
+          label: "GitHub",
+          position: "right",
         },
         {
-          href: 'https://teespring.com/glade-awkward-dark',
-          label: 'Shirts',
-          position: 'right',
-        }
+          href: "https://teespring.com/glade-awkward-dark",
+          label: "Shirts",
+          position: "right",
+        },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Getting Started',
-              to: 'docs/getting-started',
+              label: "Getting Started",
+              to: "docs/getting-started",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/gladesoftware',
+              label: "Twitter",
+              href: "https://twitter.com/gladesoftware",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Demos',
-              to: 'https://demos.glade.app',
+              label: "Demos",
+              to: "https://demos.glade.app",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/glade-software/glade-element',
+              label: "GitHub",
+              href: "https://github.com/glade-software/glade-element",
             },
           ],
         },
       ],
       copyright: `Copyright © ${new Date().getFullYear()} Glade Software, Inc. Built with Docusaurus.`,
     },
-    colorMode: {disableSwitch: true},
+    colorMode: { disableSwitch: true },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-    sidebarCollapsible: false,
+          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarCollapsible: false,
 
           // Please change this to your repo.
           editUrl:
-            'https://github.com/glade-software/glade-element/edit/master/docs/',
+            "https://github.com/glade-software/glade-element/edit/master/docs/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
-        gtag:{
-          trackingID:"G-8TKSVDNV8F"
-        }
+        gtag: {
+          trackingID: "G-8TKSVDNV8F",
+        },
       },
     ],
   ],
